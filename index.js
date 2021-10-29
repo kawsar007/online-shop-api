2132501eacdd4e58b40b10b9662327dd2d1f6d77
kawsar007 <imkawsar007@gmail.com>
2021-10-29 11:40:10 +0600

    Create Model

--- index.js
+++ index.js
@@ -2,10 +2,12 @@ const express = require('express');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 const app = express();
+const userRoute = require('./routes/user');
 
 const port = process.env.PORT || 5000;
 
 dotenv.config();
+app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URL)
 .then(() => {
@@ -17,6 +19,8 @@ mongoose.connect(process.env.MONGO_URL)
 app.get('/', (req, res) => {
     res.send("This is online shop server.")
 })
+
+app.use("/api/users", userRoute)
 
 app.listen(port, () => {
     console.log("Server is running on port ", port);
