743f6b368e9259b7056d7ed099330bbb60990f32
kawsar007 <imkawsar007@gmail.com>
2021-10-29 09:28:02 +0600

    setup home route

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const port = process.env.PORT || 5000;
 const app = express();
 
+app.get('/', (req, res) => {
+    res.send("This is online shop server.")
+})
 
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
+app.listen(port, () => {
+    console.log("Server is running on port ", port);
 })
