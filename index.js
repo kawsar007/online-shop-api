fa330c32d0986e5b18559f67fd4b0dd43e15f436
kawsar007 <imkawsar007@gmail.com>
2021-10-29 09:50:37 +0600

    Database connected successfully

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 const express = require('express');
-const port = process.env.PORT || 5000;
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
 const app = express();
+
+const port = process.env.PORT || 5000;
+
+dotenv.config();
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => {
+    console.log('DB connection successfully');
+}).catch(err => {
+    console.log(err);
+})
 
 app.get('/', (req, res) => {
     res.send("This is online shop server.")
